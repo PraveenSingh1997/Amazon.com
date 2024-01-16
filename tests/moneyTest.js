b76bbc995d1e549fd5c46db6507e023a6b3aa46c
Praveen Singh <praveen.singh@csu.fullerton.edu>
2024-01-16 03:12:12 -0800

    Created Automated test for Money conversion

---
+++ tests/moneyTest.js
@@ -0,0 +1,34 @@
+import {formatCurrency} from '../scripts/utils/money.js' ; 
+
+console.log('convert cents into dollars');
+
+// Test case 1 
+if (formatCurrency (2095) === '20.95') {
+    console.log('passed') ; 
+} else {
+    console.log('failed')
+}
+
+// Text case 2
+
+if (formatCurrency(0) === '0.00') {
+    console.log('passed'); 
+
+}else{
+    console.log('failed') ;
+}
+
+
+if (formatCurrency(2000.5) === '20.01'){
+    console.log('passed');
+}else{
+    console.log('failed') ;
+}
+
+
+if (formatCurrency(2000.4) === '20.00'){
+    console.log('passed');
+}else{
+    console.log('failed') ;
+}
+
